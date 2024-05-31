beb19c7889bc1196f3854fcc2204123495c7518b
Chandan Suri <csuri21296@gmail.com>
2024-05-30 23:00:05 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,12 +1,25 @@
 import socket
+from enum import Enum
 
+class Response(Enum):
+    OK = "HTTP/1.1 200 OK\r\n\r\n"
+    NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n"
 
 def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     connection, address = server_socket.accept() 
-    connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+
+    with connection:
+        while True:
+            requestData = connection.recv(1024)
+            requestParams = requestData.decode().split("\r\n")
+            requestURL = requestParams[0].split(" ")[1]
+
+            response = f"{Response.NOT_FOUND.value}" if requestURL != "/" else f"{Response.OK.value}"
+
+            connection.sendall(str.encode(response))
 
 
 if __name__ == "__main__":
