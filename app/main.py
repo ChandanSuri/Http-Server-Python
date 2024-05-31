bfeda4edb3594f38b220750119cde948c106e71f
Chandan Suri <csuri21296@gmail.com>
2024-05-30 23:11:57 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from enum import Enum
 class Response(Enum):
     OK = "HTTP/1.1 200 OK\r\n\r\n"
     NOT_FOUND = "HTTP/1.1 404 Not Found\r\n\r\n"
+    ECHO = "HTTP/1.1 200 OK\r\n"
 
 def main():
     print("Logs from your program will appear here!")
@@ -18,6 +19,9 @@ def main():
             requestURL = requestParams[0].split(" ")[1]
 
             response = f"{Response.NOT_FOUND.value}" if requestURL != "/" else f"{Response.OK.value}"
+            if "echo" in requestURL:
+                data = requestURL.strip("/").split("/")[1]
+                response = f"{Response.ECHO.value}Content-Type: text/plain\r\nContent-Length: {len(data)}\r\n\r\n{data}"
 
             connection.sendall(str.encode(response))
 
