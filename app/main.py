67b3a97abad0c463bb2f99ba17a218192e6f88db
Chandan Suri <csuri21296@gmail.com>
2024-05-31 10:58:01 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import socket
 from enum import Enum
+import threading
 
 class Response(Enum):
     OK = "HTTP/1.1 200 OK\r\n"
@@ -35,31 +36,33 @@ def parseRequestParams(requestParams):
 
     return requestLine, headers, body
 
+def handleRequest(connection, address):
+    requestData = connection.recv(1024)
+    requestParams = requestData.decode().split("\r\n")
+    requestLine, headers, body = parseRequestParams(requestParams)
+    requestURL = requestLine["requestUrl"]
+
+    response = f"{Response.NOT_FOUND.value}" if requestURL != "/" else f"{Response.OK.value}\r\n"
+    if "echo" in requestURL:
+        data = requestURL.strip("/").split("/")[1]
+        response = f"{Response.ECHO.value}Content-Type: text/plain\r\nContent-Length: {len(data)}\r\n\r\n{data}"
+    elif "/user-agent" == requestURL:
+        if "User-Agent" not in headers:
+            raise Exception("For /user-agent Endpoint, User Agent header is missing!")
+        userAgentHeader = headers["User-Agent"]
+        response = f"{Response.OK.value}Content-Type: text/plain\r\nContent-Length: {len(userAgentHeader)}\r\n\r\n{userAgentHeader}"
+
+    connection.sendall(str.encode(response))
+
 def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    connection, address = server_socket.accept() 
 
-    with connection:
-        while True:
-            requestData = connection.recv(1024)
-            requestParams = requestData.decode().split("\r\n")
-            requestLine, headers, body = parseRequestParams(requestParams)
-            requestURL = requestLine["requestUrl"]
+    while True:
+        connection, address = server_socket.accept() 
+        threading.Thread(target=handleRequest, args=(connection, address)).start()
 
-            response = f"{Response.NOT_FOUND.value}" if requestURL != "/" else f"{Response.OK.value}\r\n"
-            if "echo" in requestURL:
-                data = requestURL.strip("/").split("/")[1]
-                response = f"{Response.ECHO.value}Content-Type: text/plain\r\nContent-Length: {len(data)}\r\n\r\n{data}"
-            elif "/user-agent" == requestURL:
-                if "User-Agent" not in headers:
-                    raise Exception("For /user-agent Endpoint, User Agent header is missing!")
-                userAgentHeader = headers["User-Agent"]
-                response = f"{Response.OK.value}Content-Type: text/plain\r\nContent-Length: {len(userAgentHeader)}\r\n\r\n{userAgentHeader}"
-
-            connection.sendall(str.encode(response))
-
-
+           
 if __name__ == "__main__":
     main()
